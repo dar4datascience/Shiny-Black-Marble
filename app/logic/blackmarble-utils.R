410e191539bad84ee44468523ac85ba6c6bf4ecd
dar4ds <amievarodriguez.daniel@gmail.com>
2024-03-16 21:31:48 -0600

    In the original function `month_start_day_to_month`, each condition checks for a specific Julian day and assigns a corresponding month. If you wanted to apply this function to a vector of Julian dates, you would indeed need to use `Vectorize()` to make it work properly.
    However, since we've rewritten the function `julian_to_month` using vectorized operations (i.e., without explicit `if` statements), there's no need to use `Vectorize()` for this function. The `findInterval()` function is already vectorized, so it can handle multiple Julian dates efficiently without needing additional vectorization.
    
    Therefore, if you're using `julian_to_month`, you can directly apply it to a vector of Julian dates without the need for `Vectorize()`. It will handle the vectorized input efficiently.

--- app/logic/blackmarble-utils.R
+++ app/logic/blackmarble-utils.R
@@ -26,3 +26,26 @@ map_black_marble_tiles <- function(){
   return(map)
 
 }
+
+
+#' Translate Julian Dates to Regular Month Representation
+#'
+#' This function translates Julian dates to regular month representations.
+#'
+#' @param julian_date A character string representing the day of the year in Julian format (e.g., "001" for January 1st).
+#' @return A character string representing the month corresponding to the given Julian date (e.g., "01" for January).
+#' @examples
+#' julian_to_month("001")
+#' # [1] "01"
+#'
+#' julian_to_month("032")
+#' # [1] "02"
+#' @references
+#' For more information on the Julian day system, see: https://en.wikipedia.org/wiki/Julian_day
+#' @export
+julian_to_month <- function(julian_date) {
+  months <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+  days <- as.integer(julian_date)
+  month_index <- findInterval(days, c(0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366))
+  return(months[month_index])
+}
